04616906ec10bbb1d4a17a9ee098235b0096def1
Carrie Schmid and Jaime Takikawa <jaime@>
2019-06-03 20:28:45 -0700

    Add changes.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+  $("#cuttlefish").click(function() {
+    $("p").show();
+
+
+  });
+});
+
+
+  // if (cuttlefish >= 21) {
+  //   $('#drinks').show();
+  // } else {
+  //   $('#under-21').show();
+  // }
